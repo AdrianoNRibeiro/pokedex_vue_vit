3e06ae443d90ac47f3b57ec430a23c05eab95560
Adriano Neves Ribeiro <adriano.neves@lojasrede.com.br>
2024-06-14 08:27:46 -0300

    part one project pok

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,39 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue';
+
+let pokemons = reactive(ref());
+
+onMounted(()=>{
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+  .then(res => res.json())
+  .then(res => pokemons.value = res.results);
+})
 </script>
 
 <template>
   <main>
-    Home
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+          <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/921.png" class="card-img-top" alt="pokemon">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <ul>
+              <li v-for="pokemon in pokemons" :key="pokemon.name">
+                {{ pokemon.name }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </main>
 </template>
